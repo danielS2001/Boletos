2f20a8ec04a2e7b6ef8bc4fbe54ca5dfdd3b7c76
danielS2001 <saenzdaniel2001@outlook.com>
2022-05-21 21:24:02 -0600

    Primer Commit
    Se agregó el código fuente de la aplicación de "Agencia de Viajes"

---
+++ app/src/main/java/com/example/boletos/Boleto.java
@@ -0,0 +1,154 @@
+package com.example.boletos;
+
+public class Boleto {
+    //Atributos
+    private int numeroDeBoleto;
+    private String nombreDeCliente;
+    private String destino;
+    private int tipoDeViaje;
+    private String fecha;
+    private double precio;
+
+    private double impuesto;
+    private double descuento;
+    private double subTotal;
+    private double total;
+
+    //Constructores
+    public Boleto() { //Constructor vacío.
+        this.numeroDeBoleto = 0;
+        this.nombreDeCliente = "";
+        this.destino = "";
+        this.tipoDeViaje = 0;
+        this.fecha = "";
+        this.precio = 0;
+    }
+
+    public Boleto(int numeroDeBoleto, String nombreDeCliente, String destino,  //Constructor de parametros
+                  int tipoDeViaje, String fecha, double precio) {
+        this.numeroDeBoleto = numeroDeBoleto;
+        this.nombreDeCliente = nombreDeCliente;
+        this.destino = destino;
+        this.tipoDeViaje = tipoDeViaje;
+        this.fecha = fecha;
+        this.precio = precio;
+    }
+
+    public Boleto (Boleto tickets) { //Constructor de copia
+        this.numeroDeBoleto = tickets.numeroDeBoleto;
+        this.nombreDeCliente = tickets.nombreDeCliente;
+        this.destino = tickets.destino;
+        this.tipoDeViaje = tickets.tipoDeViaje;
+        this.fecha = tickets.fecha;
+        this.precio = tickets.precio;
+    }
+
+    /**
+     * @return the numeroDeBoleto
+     */
+    public int getNumeroDeBoleto() {
+        return numeroDeBoleto;
+    }
+
+    /**
+     * @param numeroDeBoleto the numeroDeBoleto to set
+     */
+    public void setNumeroDeBoleto(int numeroDeBoleto) {
+        this.numeroDeBoleto = numeroDeBoleto;
+    }
+
+    /**
+     * @return the nombreDeCliente
+     */
+    public String getNombreDeCliente() {
+        return nombreDeCliente;
+    }
+
+    /**
+     * @param nombreDeCliente the nombreDeCliente to set
+     */
+    public void setNombreDeCliente(String nombreDeCliente) {
+        this.nombreDeCliente = nombreDeCliente;
+    }
+
+    /**
+     * @return the destino
+     */
+    public String getDestino() {
+        return destino;
+    }
+
+    /**
+     * @param destino the destino to set
+     */
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    /**
+     * @return the tipoDeViaje
+     */
+    public int getTipoDeViaje() {
+        return tipoDeViaje;
+    }
+
+    /**
+     * @param tipoDeViaje the tipoDeViaje to set
+     */
+    public void setTipoDeViaje(int tipoDeViaje) {
+        this.tipoDeViaje = tipoDeViaje;
+    }
+
+    /**
+     * @return the fecha
+     */
+    public String getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+    public double calcularSubTotal() {
+        if(this.tipoDeViaje==1) subTotal = this.precio;
+        if(this.tipoDeViaje==2) subTotal = this.precio*1.8;
+        return subTotal;
+    }
+
+    public double sacarDescuento(int edad) {
+        if (edad >= 60) {
+            descuento = (subTotal) /2;
+        }
+        return descuento;
+    }
+
+    public double sacarImpuesto() {
+        impuesto = subTotal *.16;
+        subTotal = subTotal + impuesto;
+        return impuesto;
+    }
+
+    public double sacarTotal() {
+        total = subTotal - descuento;
+        return total;
+    }
+}
---
+++ app/src/main/java/com/example/boletos/MainActivity.java
@@ -0,0 +1,175 @@
+package com.example.boletos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtNumeroDeBoleto;
+    private TextView lblDatos;
+
+    private EditText txtNombreDeCliente;
+    private EditText txtEdad;
+    private TextView lblDestino;
+    private Spinner spnDestino;
+    private TextView lblTipoDeViaje;
+    private Spinner spnTipoDeViaje;
+    private EditText txtFecha;
+    private EditText txtPrecio;
+    private Button btnIntroducirDatos;
+    private Button btnMostrarDatos;
+    private Button btnRegresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtNumeroDeBoleto=(EditText) findViewById(R.id.txtNumeroDeBoleto);
+        lblDatos = (TextView) findViewById(R.id.lblDatos);
+
+        txtNombreDeCliente=(EditText) findViewById(R.id.txtNombreDeCliente);
+        txtEdad = (EditText) findViewById(R.id.txtEdad);
+        lblDestino = (TextView) findViewById(R.id.lblDestino);
+        spnDestino=(Spinner) findViewById(R.id.spnDestino);
+        lblTipoDeViaje=(TextView) findViewById(R.id.lblTipoDeViaje);
+        spnTipoDeViaje=(Spinner) findViewById(R.id.spnTipoDeViaje);
+        txtFecha=(EditText) findViewById(R.id.txtFecha);
+        txtPrecio=(EditText) findViewById(R.id.txtPrecio);
+
+        btnIntroducirDatos=(Button) findViewById(R.id.btnIntroducirDatos);
+        btnMostrarDatos=(Button) findViewById(R.id.btnMostrarDatos);
+        btnRegresar = (Button) findViewById(R.id.btnRegresar);
+
+        final String[] destino = {""};
+        final int[] tipoDeViaje = {0};
+        final double[] subTotal = new double[1];
+        final double[] descuento = new double[1];
+        final double[] impuesto = new double[1];
+        final double[] total = new double[1];
+
+        Boleto boletos = new Boleto();
+
+        btnIntroducirDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boletos.setNombreDeCliente(txtNombreDeCliente.getText().toString());
+
+                String numeroDeBoleto = txtNumeroDeBoleto.getText().toString();
+                boletos.setNumeroDeBoleto(Integer.parseInt(numeroDeBoleto));
+
+                String edad = txtEdad.getText().toString();
+
+                boletos.setDestino(destino[0]);
+                boletos.setTipoDeViaje(tipoDeViaje[0]);
+                boletos.setFecha(txtFecha.getText().toString());
+                String precio = txtPrecio.getText().toString();
+                boletos.setPrecio(Double.parseDouble(precio));
+
+                //Funciones
+                subTotal[0] = boletos.calcularSubTotal();
+                descuento[0] = boletos.sacarDescuento(Integer.parseInt(edad));
+                impuesto[0] = boletos.sacarImpuesto();
+                total[0] = boletos.sacarTotal();
+
+                Toast.makeText(MainActivity.this,"Datos ingresados",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnMostrarDatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                txtNombreDeCliente.setVisibility(View.GONE);
+                txtNumeroDeBoleto.setVisibility(View.GONE);
+                txtEdad.setVisibility(View.GONE);
+                lblDestino.setVisibility(View.GONE);
+                spnDestino.setVisibility(View.GONE);
+                lblTipoDeViaje.setVisibility(View.GONE);
+                spnTipoDeViaje.setVisibility(View.GONE);
+                txtFecha.setVisibility(View.GONE);
+                txtPrecio.setVisibility(View.GONE);
+                btnIntroducirDatos.setVisibility(View.GONE);
+                btnMostrarDatos.setVisibility(View.GONE);
+                btnRegresar.setVisibility(View.VISIBLE);
+
+                lblDatos.setText("Nombre del cliente: " +boletos.getNombreDeCliente()
+                +"\nNumero de Boleto: " +txtNumeroDeBoleto.getText().toString()
+                +"\nEdad: " +txtEdad.getText().toString()
+                +"\nDestino: " +boletos.getDestino()
+                +"\nTipo de viaje: " +boletos.getTipoDeViaje()
+                +"\nFecha: " +boletos.getFecha()
+                +"\n\nPrecio: " +boletos.getPrecio()
+                + "\n\nSubTotal: " +subTotal[0]
+                + "\n\nDescuento: " +descuento[0]
+                + "\n\nImpuesto: " +impuesto[0]
+                + "\n\nTotal: " +total[0]
+                );
+            }
+        });
+
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lblDatos.setText("");
+
+                txtNombreDeCliente.setText("");
+                txtNumeroDeBoleto.setText("");
+                txtEdad.setText("");;
+                txtFecha.setText("");
+                txtPrecio.setText("");
+
+                txtNombreDeCliente.setVisibility(View.VISIBLE);
+                txtNumeroDeBoleto.setVisibility(View.VISIBLE);
+                txtEdad.setVisibility(View.VISIBLE);
+                lblDestino.setVisibility(View.VISIBLE);
+                spnDestino.setVisibility(View.VISIBLE);
+                lblTipoDeViaje.setVisibility(View.VISIBLE);
+                spnTipoDeViaje.setVisibility(View.VISIBLE);
+                txtFecha.setVisibility(View.VISIBLE);
+                txtPrecio.setVisibility(View.VISIBLE);
+                btnIntroducirDatos.setVisibility(View.VISIBLE);
+                btnMostrarDatos.setVisibility(View.VISIBLE);
+                btnRegresar.setVisibility(View.GONE);
+            }
+        });
+
+        //En esta sección se programa el spinner de destinos
+        ArrayAdapter<String> Adaptador=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,getResources().getStringArray(R.array.destinos));
+        spnDestino.setAdapter(Adaptador);
+        spnDestino.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                destino[0] = adapterView.getItemAtPosition(i).toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }});
+
+        //En esta sección se programa el spinner del tipo de viaje
+        ArrayAdapter<String> Adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_expandable_list_item_1,getResources().getStringArray(R.array.tiposDeViajes));
+        spnTipoDeViaje.setAdapter(Adapter);
+        spnTipoDeViaje.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String kindOfJourney = adapterView.getItemAtPosition(i).toString();
+                if (kindOfJourney.matches("1. Sencillo")) {
+                    tipoDeViaje[0] = 1;
+                } else {
+                    tipoDeViaje[0] = 2;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }});
+
+        //txtNumeroDeBoleto.setText("1");
+    }
+}
